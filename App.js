ea0f6f6dec1408614f684b26065d4b74d883b046
Kartik Goel <goelkartik235@gmail>
2024-02-22 19:56:42 +0530

    update

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-//const heading = React.createElement('h1', {id: "heading"}, "Kartik Goel ");
+const Header = () =>{
+    return (
+        <div className='header'>
+            <div className='logo-container'>
+                <img 
+                className='logo'
+                src="https://imgs.search.brave.com/RrOhvqioSFYMakDEmNj8hU0alqCyxxbWht-_vXsUURM/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9sb2dv/LmNvbS9pbWFnZS1j/ZG4vaW1hZ2VzL2t0/czkyOHBkL3Byb2R1/Y3Rpb24vN2FiNWRl/ZjBhYjFkYWQyNmE5/MGJjMzVjYjdlZWQ5/ZTYyOGY3ZjIwMS00/MzB4NDMwLnBuZz93/PTEwODAmcT03Mg" alt='logo'
+                />
+            </div>
 
-// JSX (transpiled before it reaches the JS) - PARCEL - Babel
-const jsxheading = <h1 id="heading">Kartik written in jsx</h1>;
+            <div className='nav-items'>
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+const AppLayout = () =>{
+     return <div className = "app">
+        <Header/>
+     </div>    
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(jsxheading);
+root.render(<AppLayout />);
